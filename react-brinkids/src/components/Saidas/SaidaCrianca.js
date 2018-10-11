3130fe236f27272dd0643c9a9ce215209c0ef0c0
AtysonJaime <atysonjaime@gmail.com>
2018-10-10 23:54:33 -0300

    Update da Tela Saida Criança

--- react-brinkids/src/components/Saidas/SaidaCrianca.js
+++ react-brinkids/src/components/Saidas/SaidaCrianca.js
@@ -13,15 +13,29 @@ class SaidaCrianca extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            page: "UsuarioAdulto",
+            page: "MostraCrianca",
             listAdultos: [],
             listCrianca: [],
             CriancasSelecionadas: [],
-            NameAdult: "João",
-            PhoneAdult: "900000000",
-            CPFAdult: "000.000.000-00",
-            ObsAdult: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            
+            //Adulto
+            NameAdult: "",
+            PhoneAdult: "",
+            CPFAdult: "",
+            ObsAdult: "",
             PhotoAdult: "",
+            //Crianca
+            Interacao: 1,
+            NomeC: "",
+            Produto: "",
+            PhotoC: "",
+            IdadeC: "",
+            TempoC: "",
+            ObsC: "",
+            RetC: "",
+            Valor: "",
+            Desconto: "",
+
         }
 
         this.ChangeValue = this.ChangeValue.bind(this);
@@ -111,15 +125,36 @@ class SaidaCrianca extends React.Component {
     }
     ProximaTela = (event) =>{
         event.preventDefault();
-
+        var erros = [];
         if(this.state.CriancasSelecionadas.length > 0){
             this.setState({
-
+                NomeC: this.state.CriancasSelecionadas[0].children.name,
+                Produto: this.state.CriancasSelecionadas[0].service,
+                PhotoC: this.state.CriancasSelecionadas[0].photo,
+                IdadeC: this.state.CriancasSelecionadas[0].children.birthday,
+                TempoC: this.state.CriancasSelecionadas[0].time,
+                ObsC: this.state.CriancasSelecionadas[0].children.observations,
+                RetC: this.state.CriancasSelecionadas[0].children.restrictions,
+                page: "MostraCrianca",
             })
         }
         else {
-            alert("Selecione Alguma Criança");
+            erros.push("Selecione ao menos uma criança, por favor");
+            exibeMensagensDeErro(erros);
         }
+        function exibeMensagensDeErro(erros){
+            var ul = document.querySelector("#mensagens-erro");
+            ul.innerHTML = "";
+
+            erros.forEach(function(erro){
+                var li = document.createElement("li");
+                li.textContent = erro;
+                ul.appendChild(li);
+            });
+        }
+    }
+    VerificaDesconto = (resp) => {
+        console.log("Estou entrando");
     }
 
 
@@ -199,6 +234,7 @@ class SaidaCrianca extends React.Component {
                         </ol>
                     </div>
                     <div className="graph-visual" >
+                        <h3>Responsavel Escolhido</h3>
                         <div className="graph" >
                             <div className="row">
                                 <div className="col-md-4 col-sm-12 com-xs-12">
@@ -262,7 +298,7 @@ class SaidaCrianca extends React.Component {
                                             <tr>
                                                 <th style={{ fontSize: 20 + 'px' }}>2</th>
                                                 <td className="td-table text-center">Maria</td>
-                                                <td className="td-table text-center">(84) 900000000</td>
+                                                <td className="td-table text-center">Aniversario</td>
                                                 <td className="td-table text-center"> 10/10/2018 - 12:00</td>
                                                 <td>
                                                     <input type="checkbox" name="selectchild" value="true" onClick={() => this.selecionaCrianca("abacate")} />
@@ -271,7 +307,7 @@ class SaidaCrianca extends React.Component {
                                             <tr>
                                                 <th style={{ fontSize: 20 + 'px' }}>3</th>
                                                 <td className="td-table text-center">Firmino</td>
-                                                <td className="td-table text-center">(84) 999798909</td>
+                                                <td className="td-table text-center">Aniversario</td>
                                                 <td className="td-table text-center"> 10/10/2018 - 12:00</td>
                                                 <td>
                                                     <input type="checkbox" name="selectchild" value="true" onClick={() => this.selecionaCrianca("abacate")} />
@@ -294,9 +330,91 @@ class SaidaCrianca extends React.Component {
                                     <div className="text-center">
                                         <a className="btn btn-md botao" href="/">Cancelar</a>
                                         <button className="btn btn-md botao botaoAvançar" onClick={this.ProximaTela}>Proximo</button>
+                                        <ul id="mensagens-erro" style={{color: "red"}}></ul>
                                     </div>  
                                 </form>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        else if (this.state.page === "MostraCrianca"){
+            return (
+                <div className="container-fluid" >
+                    <div className="sub-heard-part" >
+                        <ol className="breadcrumb m-b-0" >
+                            <li > < a href="/" > Home </a></li >
+                            <li > Saida de Criança </li>
+                        </ol>
+                    </div>
+                    <div className="graph-visual" >
+                        <h3>Apresentar Crianças</h3>
+                        <div className="graph" >
+                            <div className="row">
+                                <div className="col-md-6 col-sm-12 col-xs-12">
+                                    <div className="graph" style={{ padding: 10 + "px" }}>
+                                        <h5 className="ltTitulo"><b> Sua Foto: </b></h5>
+                                        <img src={"http://localhost:3000/img-users/" + this.state.PhotoC} />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="graph">
+                                        <h5 className="ltTitulo"><b>Nome:</b></h5>
+                                        <p>{this.state.NomeC}</p>
+                                    </div>
+                                    <br></br>
+                                    <div className="graph">
+                                        <h5 className="ltTitulo"><b>Idade:</b></h5>
+                                        <p>{this.state.IdadeC}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="row">
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="graph" style = {{ padding:10 + "px"}}>
+                                        <h5 className = "ltTitulo"><b> Observações: </b></h5>
+                                        <p>{this.state.ObsAdult}</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="graph" style = {{ padding:10 + "px"}}>
+                                        <h5 className = "ltTitulo"><b> Restrições: </b></h5>
+                                        <p>{this.state.ObsAdult}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="row">
+                                <div className="col-md-4 col-sm-12 com-xs-12">
+                                    <div className="graph">
+                                        <h5 className="ltTitulo"><b>Tempo:</b></h5>
+                                        <p>{this.state.NameAdult}</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4 col-sm-12 com-xs-12">
+                                    <div className="graph">
+                                        <h5 className="ltTitulo"><b>Produto:</b></h5>
+                                        <p>{this.state.Produto}</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-4 col-sm-12 com-xs-12">
+                                    <div className="graph">
+                                        <h5 className="ltTitulo"><b>Valor:</b></h5>
+                                        <p>{this.state.Valor}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br></br>
+                        <div className="graph">
+                            <form>
+                                <TypesInput cod = {1} ClassDiv = {"col-md-12 col-sm-12"} ClassLabel = {"LetraFormulario"} NameLabel = {"Codigo do Desconto:"} type = {"text"} id = {"cod"} name= {"Desconto"} Class = {"form-control"} value = {this.state.Desconto}onChange={this.ChangeValue}/>
+                                <div className ="text-center">
+                                    <button className="btn btn-md botao" onClick={() => this.VerificaDesconto(this.state.Desconto)}>Validar Desconto</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
